6dbbba83bbb4ea32321a94ea92123223b8555a81
Aiden Rathsam <rathsamaiden@gmail.com>
2025-10-02 17:18:50 -0400

    completed colored btn change

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ function createMultTable(){
     }
     div.appendChild(lineBreak);
 }
+function getRandColor(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+} 
+let colorArr = [];
+document.addEventListener('DOMContentLoaded', () => {
+    let btns = document.querySelectorAll(`.colorBtn`);
+    btns.forEach(function(button){
+        let color = getRandColor()
+        colorArr.push(color);
+        button.style.backgroundColor = color;
+    });
+});
+function btnColorChange(thing){
+    console.log(thing);
+    if(thing.id === `redChange`){
+        let btns = document.querySelectorAll(`.colorBtn`);
+        btns.forEach(function(button){
+            button.style.backgroundColor = `rgb(255,0,0)`;
+        });
+    }
+    if(thing.id === `greenChange`){
+        let btns = document.querySelectorAll(`.colorBtn`);
+        btns.forEach(function(button){
+            button.style.backgroundColor = `rgb(0,255,0)`;
+        });
+    }
+    if(thing.id === `blueChange`){
+        let btns = document.querySelectorAll(`.colorBtn`);
+        btns.forEach(function(button){
+            button.style.backgroundColor = `rgb(0,0,255)`;
+        });
+    }
+    if(thing.id === `randomChange`){
+        let btns = document.querySelectorAll(`.colorBtn`);
+        btns.forEach(function(button){
+            button.style.backgroundColor = getRandColor();
+        });
+    }
+    if(thing.id === `reset`){
+        let btns = document.querySelectorAll(`.colorBtn`);
+        let index = 0;
+        btns.forEach(function(button){
+            button.style.backgroundColor = colorArr[++index];
+        });
+    }
+}
